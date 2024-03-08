42f44e30ee843a6c245ea3ffce49fe87f5bbc762
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-03-08 22:15:30 +0700

    main

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,20 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  MyApp app = MyApp();
+  runApp(app);
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
+class MyApp extends StatelessWidget {}
