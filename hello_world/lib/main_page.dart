4eedd8bebab4318b6989a60d6afe2ed4e4717ed2
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-03-11 15:18:30 +0700

    container

--- hello_world/lib/main_page.dart
+++ hello_world/lib/main_page.dart
@@ -6,23 +6,69 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Aplikasi Pertamaku'),
-        ),
-        //widget center
-        // body: const Center(
-        //   child: Text("Hello World!"),
-        // ),
-        body: const Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text("Hello world"),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [Text('Hi World'), Text('Hola World')],
-            ),
-            Text("Another Hello")
-          ],
-        ));
+      appBar: AppBar(
+        title: const Text('Aplikasi Pertamaku'),
+      ),
+      //widget center
+
+      // body: const Center(
+      //   child: Text("Hello World!"),
+      // ),
+
+      //Column and row
+      // body: const Column(
+      //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //   children: [
+      //     Text("Hello world"),
+      //     Row(
+      //       mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //       children: [Text('Hi World'), Text('Hola World')],
+      //     ),
+      //     Text("Another Hello")
+      //   ],
+      // )
+
+      //Container
+      body: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+                margin: const EdgeInsets.symmetric(vertical: 8.0),
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.orange,
+                ),
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.green,
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
