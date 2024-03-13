1e0c2805a3ef1715f61178af446f5ae8557898e0
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-03-13 20:00:52 +0700

    text widget

--- hello_world/lib/text_widget.dart
+++ hello_world/lib/text_widget.dart
@@ -20,14 +20,20 @@ class TextWidget extends StatelessWidget {
           maxLines: 2,
           overflow: TextOverflow.ellipsis,
           style: TextStyle(
-              backgroundColor: Colors.red,
-              color: Colors.white,
+              //backgroundColor: Colors.red,
+              color: Colors.black,
               fontSize: 20,
-              fontWeight: FontWeight.w300,
+              fontWeight: FontWeight.w500,
               fontStyle: FontStyle.italic,
               shadows: [
-                Shadow(blurRadius: 5, color: Colors.black, offset: Offset(3, 3))
-              ]),
+                Shadow(blurRadius: 5, color: Colors.grey, offset: Offset(3, 3))
+              ],
+              //letterSpacing: 5,
+              //wordSpacing: 5,
+              decoration: TextDecoration.underline,
+              decorationColor: Colors.orange,
+              decorationStyle: TextDecorationStyle.solid,
+              decorationThickness: 2),
         ),
       ),
     );
