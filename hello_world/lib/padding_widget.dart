3c4ff01b6367d82ea7eec0ebba434e2b4791767f
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-03-14 20:03:05 +0700

    padding

---
+++ hello_world/lib/padding_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+/**
+ * 
+ * Padding: ukurannya lebih kecil daripada Container karna cuma punya 1 properti jadi lebih irit memori
+ * memiliki const constructor, jadi bisa digunakan berkali-kali untuk pointing ke objek yang sama.
+ */
+class PaddingWidget extends StatelessWidget {
+  const PaddingWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+  }
+}
