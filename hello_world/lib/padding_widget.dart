80990ff49bb5b3be76cc711317492398b34266c3
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-03-15 21:30:36 +0700

    padding

--- hello_world/lib/padding_widget.dart
+++ hello_world/lib/padding_widget.dart
@@ -4,7 +4,12 @@ import 'package:flutter/material.dart';
  * 
  * Padding: ukurannya lebih kecil daripada Container karna cuma punya 1 properti jadi lebih irit memori
  * memiliki const constructor, jadi bisa digunakan berkali-kali untuk pointing ke objek yang sama.
+ Improve the code:
+ -Bisa pakai wrap -> list.generate
+ -Bisa pakai helper method, menerima String text
+ 
  */
+
 class PaddingWidget extends StatelessWidget {
   const PaddingWidget({super.key});
 
@@ -12,30 +17,42 @@ class PaddingWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Container(
-          color: Colors.red,
-          child: const Padding(
-            padding: EdgeInsets.fromLTRB(20, 20, 10, 20),
-            child: Text('Lorem'),
-          ),
-        ),
-        const Padding(
-          padding: EdgeInsets.fromLTRB(20, 20, 10, 20),
-          child: Text('Ipsum'),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(20, 20, 10, 20),
-          child: Text('Dolor'),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(20, 20, 10, 20),
-          child: Text('Sit'),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(20, 20, 10, 20),
-          child: Text('Amet'),
-        ),
+        // Container(
+        //   color: Colors.red,
+        //   child: Padding(
+        //     padding: EdgeInsets.fromLTRB(20, 20, 10, 20),
+        //     child: Text('Lorem'),
+        //   ),
+        // ),
+        // Padding(
+        //   padding: EdgeInsets.fromLTRB(20, 20, 10, 20),
+        //   child: Text('Ipsum'),
+        // ),
+        // Padding(
+        //   padding: EdgeInsets.fromLTRB(20, 20, 10, 20),
+        //   child: Text('Dolor'),
+        // ),
+        // Padding(
+        //   padding: EdgeInsets.fromLTRB(20, 20, 10, 20),
+        //   child: Text('Sit'),
+        // ),
+        // Padding(
+        //   padding: EdgeInsets.fromLTRB(20, 20, 10, 20),
+        //   child: Text('Amet'),
+        // ),
+        buildPadding("Lorem"),
+        buildPadding("Ipsum"),
+        buildPadding("Dolor"),
+        buildPadding("Sit"),
+        buildPadding("Amet")
       ],
+    );
+  }
+
+  Widget buildPadding(String text) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(20, 20, 10, 20),
+      child: Text(text),
     );
   }
 }
