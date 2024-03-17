1517476c28e8afb437d2d9a94dec217a4a7f82c2
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-03-17 15:43:46 +0700

    input decoration

---
+++ hello_world/lib/input_decoration.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class InputDecorWidget extends StatefulWidget {
+  const InputDecorWidget({super.key});
+
+  @override
+  State<InputDecorWidget> createState() => _InputDecorWidgetState();
+}
+
+class _InputDecorWidgetState extends State<InputDecorWidget> {
+//Take val
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
