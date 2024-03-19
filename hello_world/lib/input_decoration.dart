cbfb68bd7df9155c00ba770efe0966957cb76e67
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-03-19 21:25:42 +0700

    fix padding

--- hello_world/lib/input_decoration.dart
+++ hello_world/lib/input_decoration.dart
@@ -38,97 +38,92 @@ class _InputDecorWidgetState extends State<InputDecorWidget> {
         children: [
           TextField(
             decoration: InputDecoration(
-              hintText: "Write Something Here - ini hintText",
-              hintMaxLines: 2,
-              hintStyle: const TextStyle(
-                  height: 3, fontWeight: FontWeight.w100, fontSize: 12),
-              labelStyle: const TextStyle(
-                  height: 0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 12),
-              //declare labelText gaboleh barengan sama label
-              //labelText: "Input something here",
-              label: Container(
-                transform: Matrix4.translationValues(0, 0, 0),
-                padding: EdgeInsets.all(3),
-                color: Colors.red[200],
-                child: Text('Input Something Here - Ini label'),
-              ),
-              floatingLabelAlignment: FloatingLabelAlignment.center,
-              floatingLabelBehavior: FloatingLabelBehavior.auto,
-              helperText: "Ini helper text",
-              helperStyle: TextStyle(
-                  color: Colors.red.shade200, fontWeight: FontWeight.bold),
-
-              //Counter - hitung berapa kata yang sudah diinput
-              counter: Container(
-                margin: const EdgeInsets.only(top: 5),
-                child: Text(
-                  '${textEditingController.text.length}/100',
-                  style: const TextStyle(
-                      color: Colors.grey, fontWeight: FontWeight.normal),
+                hintText: "Write Something Here - ini hintText",
+                hintMaxLines: 2,
+                hintStyle: const TextStyle(
+                    height: 3, fontWeight: FontWeight.w100, fontSize: 12),
+                labelStyle: const TextStyle(
+                    height: 0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12),
+                //declare labelText gaboleh barengan sama label
+                //labelText: "Input something here",
+                label: Container(
+                  transform: Matrix4.translationValues(0, 0, 0),
+                  padding: EdgeInsets.all(3),
+                  color: Colors.red[200],
+                  child: Text('Input Something Here - Ini label'),
                 ),
-              ),
-              //Counter property lainnya
-              // counterText: '${textEditingController.text.length}/100',
-              // counterStyle: const TextStyle(
-              //     color: Colors.grey, fontWeight: FontWeight.normal)
+                floatingLabelAlignment: FloatingLabelAlignment.center,
+                floatingLabelBehavior: FloatingLabelBehavior.auto,
+                helperText: "Ini helper text",
+                helperStyle: TextStyle(
+                    color: Colors.red.shade200, fontWeight: FontWeight.bold),
 
-              //errortext
-              // errorText: "This is error text",
-              // errorStyle: const TextStyle(
-              //   color: Colors.pink,
-              error: Container(
-                  child: const Text(
-                'This is error text',
-                style: TextStyle(color: Colors.pink),
-              )),
+                //Counter - hitung berapa kata yang sudah diinput
+                counter: Container(
+                  margin: const EdgeInsets.only(top: 5),
+                  child: Text(
+                    '${textEditingController.text.length}/100',
+                    style: const TextStyle(
+                        color: Colors.grey, fontWeight: FontWeight.normal),
+                  ),
+                ),
+                //Counter property lainnya
+                // counterText: '${textEditingController.text.length}/100',
+                // counterStyle: const TextStyle(
+                //     color: Colors.grey, fontWeight: FontWeight.normal)
 
-              //Icon
-              //Icon - nerima widget juga incl. Container
-              icon: Padding(
-                padding: const EdgeInsets.only(
-                    top: 30), // Adjust the top padding as needed
-                child: Icon(MdiIcons.faceManProfile),
-              ),
-              //Prefix
-              prefixIcon: Padding(
-                padding: const EdgeInsets.only(top: 15),
-                child: Icon(
-                  MdiIcons.home,
+                //errortext
+                // errorText: "This is error text",
+                // errorStyle: const TextStyle(
+                //   color: Colors.pink,
+                error: Container(
+                    child: const Text(
+                  'This is error text',
+                  style: TextStyle(color: Colors.pink),
+                )),
+
+                //Icon
+                //Icon - nerima widget juga incl. Container
+                icon: Padding(
+                  padding: const EdgeInsets.only(
+                      top: 30), // Adjust the top padding as needed
+                  child: Icon(MdiIcons.faceManProfile),
+                ),
+                //Prefix
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: Icon(
+                    MdiIcons.home,
+                    size: 20,
+                  ),
+                ),
+                prefixIconColor: Colors.red.shade200,
+                prefix:  Text('Prefix:'),
+                
+
+                //Suffix
+                suffixIcon: Icon(
+                  MdiIcons.coffee,
                   size: 20,
                 ),
-              ),
-              prefixIconColor: Colors.red.shade200,
-              prefix: Container(
-                padding: EdgeInsets.all(5),
-                child: Text('Prefix:'),
-              ),
+                suffixIconColor: Colors.red.shade200,
+                suffix: Container(
+                  padding: EdgeInsets.all(5),
+                  child: Text('Suffix'),
+                ),
 
-              //Suffix
-              suffixIcon: Icon(
-                MdiIcons.coffee,
-                size: 20,
-              ),
-              suffixIconColor: Colors.red.shade200,
-              suffix: Container(
-                padding: EdgeInsets.all(5),
-                child: Text('Suffix'),
-              ),
+                //FillColor
+                fillColor: Colors.grey.shade200,
+                filled: true,
 
-              //FillColor
-              fillColor: Colors.grey.shade200,
-              filled: true,
+                //border
+                //enabledBorder: InputBorder.none,
 
-              //border
-              //enabledBorder: InputBorder.none,
-
-              
-              
-              
-              ),
-            ),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.yellow.shade300))),
 
             style: const TextStyle(fontWeight: FontWeight.bold),
             textAlign: TextAlign.start,
