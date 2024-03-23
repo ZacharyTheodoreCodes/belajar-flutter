fa3839ea49f18d640388e70f7551cb0478b0a878
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-03-23 17:12:42 +0700

    animated container 2

---
+++ hello_world/lib/animated_container2.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class animatedContainer2Widget extends StatefulWidget {
+  const animatedContainer2Widget({super.key});
+
+  @override
+  State<animatedContainer2Widget> createState() =>
+      _animatedContainer2WidgetState();
+}
+
+class _animatedContainer2WidgetState extends State<animatedContainer2Widget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
