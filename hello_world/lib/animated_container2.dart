c6b7c2668aae0058c6321782ae722a1fd2293e37
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-03-23 18:00:55 +0700

    animated container 2

--- hello_world/lib/animated_container2.dart
+++ hello_world/lib/animated_container2.dart
@@ -12,7 +12,9 @@ class _animatedContainer2WidgetState extends State<animatedContainer2Widget> {
   //variable to control width of the container
   //_ means private identifiers
   //extends : inheritance,
+  
   double _width = 100.0;
+  Color buttonColor = Colors.white;
 
   void _toggleWidth() {
     setState(() {
@@ -26,6 +28,7 @@ class _animatedContainer2WidgetState extends State<animatedContainer2Widget> {
       child: Column(
         children: [
           AnimatedContainer(
+            alignment: Alignment.center,
             duration: Duration(seconds: 1),
             width: _width,
             height: 100.0,
@@ -35,14 +38,37 @@ class _animatedContainer2WidgetState extends State<animatedContainer2Widget> {
               child: Text(
                 'Animated Container',
                 style: TextStyle(color: Colors.white),
+                textAlign: TextAlign.center,
               ),
             ),
           ),
           const SizedBox(
             height: 20.0,
           ),
-          ElevatedButton(
-              onPressed: _toggleWidth, child: const Text('toggle width'))
+          MouseRegion(
+            cursor: SystemMouseCursors.click,
+            onEnter: (_) {
+              setState(() {
+                // Change button appearance when hovered
+                // For example, change the button color or add a shadow
+                buttonColor = Colors.red;
+              });
+            },
+            onExit: (_) {
+              setState(() {
+                // Revert back to the original appearance when not hovered
+                buttonColor = Colors.blue;
+              });
+            },
+            child: ElevatedButton(
+              onPressed: _toggleWidth,
+              child: const Text('toggle width'),
+              style: ButtonStyle(backgroundColor:
+                  MaterialStateProperty.resolveWith<Color>((states) {
+                return buttonColor; // Use the buttonColor variable here
+              })),
+            ),
+          ),
         ],
       ),
     );
