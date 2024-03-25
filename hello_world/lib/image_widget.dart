5112ef0005d0e6e7c52d845295ecf32ba176b088
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-03-25 21:01:30 +0700

    image widget

--- hello_world/lib/image_widget.dart
+++ hello_world/lib/image_widget.dart
@@ -9,13 +9,14 @@ class ImageWidget extends StatelessWidget {
         child: Container(
       width: 200,
       height: 200,
-      padding: const EdgeInsets.all(5.0),
+      padding: const EdgeInsets.all(8.0),
       color: Colors.red.shade300,
       child: Image(
         image: NetworkImage(
           'https://press.tiffany.com/wp-content/uploads/02_0525_v4_QC_R600-1095x900.jpg',
         ),
-        fit: BoxFit.cover,
+        fit: BoxFit.contain,
+        repeat: ImageRepeat.repeat,
       ),
     ));
   }
