1e998be5f7d9f7db8d4042b7f2a1ecc9adef2a16
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-03-25 21:06:58 +0700

    image widget

--- hello_world/lib/image_widget.dart
+++ hello_world/lib/image_widget.dart
@@ -11,10 +11,15 @@ class ImageWidget extends StatelessWidget {
       height: 200,
       padding: const EdgeInsets.all(8.0),
       color: Colors.red.shade300,
+      // child: const Image(
+      //   image: NetworkImage(
+      //     'https://press.tiffany.com/wp-content/uploads/02_0525_v4_QC_R600-1095x900.jpg',
+      //   ),
+      //   fit: BoxFit.contain,
+      //   repeat: ImageRepeat.repeat,
+      // ),
       child: Image(
-        image: NetworkImage(
-          'https://press.tiffany.com/wp-content/uploads/02_0525_v4_QC_R600-1095x900.jpg',
-        ),
+        image: AssetImage("assets/sulwhasoo-blackpink-rose-5.jpeg"),
         fit: BoxFit.contain,
         repeat: ImageRepeat.repeat,
       ),
