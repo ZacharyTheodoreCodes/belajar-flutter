32e21b7b41b8c364516ca4cc65149eb0e496465d
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-03-26 20:58:28 +0700

    multi page flutter

---
+++ hello_world/lib/navigation_multiplePage/main_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MainMultiPage extends StatelessWidget {
+  const MainMultiPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('hello world');
+  }
+}
+
+/**
+ * hello_world/lib/navigation_multiplePage
+ */
